35d24c663bf3284982e6e4d84b2479ceec5da7ef
Rafael Brendo <rafalive122@hotmail.com>
2024-04-12 20:29:14 -0300

    Diminuindo a verbosidade

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,15 +24,24 @@ function convertPokemonToLi(pokemon){
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons)=> {
-        const listItems = []
-
+       
+        // const newList = pokemons.map((pokemon)=> convertPokemonToLi(pokemon))
+        // const newList = pokemons.map(convertPokemonToLi).join('')
         
+        
+        // const newHtml = newList.join('')
+        // passo a string vazia para não aparecer a virgula
 
-        for (let i =0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            listItems.push(convertPokemonToLi(pokemon))
-        }
-            
-            console.log(listItems)
+        //diminuindo a verbosidade 
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+
+        //código antigo:
+        // const listItems = []
+        // for (let i =0; i < pokemons.length; i++) {
+        //     const pokemon = pokemons[i];
+        //     listItems.push(convertPokemonToLi(pokemon))
+        // } 
+        //     console.log(listItems)
     })
     
